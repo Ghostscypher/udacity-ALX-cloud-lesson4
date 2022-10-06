9f0a3cb7e1cb66bc6b82bd2ff544caeb515dcd68
Ghostscypher <bngetich69@gmail.com>
2022-10-06 11:20:15 +0300

    Fix: add url to DB whenever upload url is requested + add Xray tracing to dynamoDB accessors

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -1,6 +1,7 @@
 import * as AWS from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
 import { createLogger } from '../utils/logger'
+import { TodosAccess } from './todosAcess';
 
 if (process.env.IS_OFFLINE) {
     AWSXRay.setContextMissingStrategy("LOG_ERROR");
@@ -17,8 +18,15 @@ export function getAttachmentUrl(todoId: string) {
     return `https://${bucketName}.s3.amazonaws.com/${todoId}`
 }
 
-export async function createAttachmentPresignedUrl(todoId: string) {
+export async function createAttachmentPresignedUrl(todoId: string, userId: string) {
     logger.info('createAttachmentPresignedUrl', { todoId })
+
+    // Update attachment url in dynamodb
+    const url = getAttachmentUrl(todoId)
+    logger.info('createAttachmentPresignedUrl', { url })
+
+    const toDoAccessor = new TodosAccess();
+    await toDoAccessor.updateAttachmentUrl(todoId, url, userId)
 
     return s3.getSignedUrl('putObject', {
         Bucket: bucketName,
